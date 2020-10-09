82fe219c452a245709a538173cdfa9f72febff47
JaeHwan <ahhancom@gmail.com>
2020-10-09 20:59:12 +0900

    타입스크립트 시작

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "jaehwan", age = 25, gender = "man";
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, ${age},${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
